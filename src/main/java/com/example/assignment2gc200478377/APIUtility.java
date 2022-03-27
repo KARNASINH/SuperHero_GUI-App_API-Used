8f914d6b9b8975b37108cd6e5d45f87f10837251
KARNASINH <karnasinh281@gmail.com>
2022-03-27 17:45:18 -0400

    Initial commit, Project setup is done.

---
+++ src/main/java/com/example/assignment2gc200478377/APIUtility.java
@@ -0,0 +1,5 @@
+package com.example.assignment2gc200478377;
+
+public class APIUtility
+{
+}
---
+++ src/main/java/com/example/assignment2gc200478377/StockSearchViewController.java
@@ -0,0 +1,16 @@
+package com.example.assignment2gc200478377;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StockSearchViewController
+{
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick()
+    {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
