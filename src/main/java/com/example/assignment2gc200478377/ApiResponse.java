192b145b8852109bbec2afcd65ce7cc0608f3bc4
KARNASINH <karnasinh281@gmail.com>
2022-03-28 12:15:03 -0400

    Created an API response class.

--- src/main/java/com/example/assignment2gc200478377/ApiResponse.java
+++ src/main/java/com/example/assignment2gc200478377/ApiResponse.java
@@ -1,5 +1,23 @@
 package com.example.assignment2gc200478377;
+import com.google.gson.annotations.SerializedName;
 
 public class ApiResponse
 {
+    @SerializedName("Meta Data")
+    private StockDetail[] metaData;
+
+    @SerializedName("Time Series (Daily)")
+    private HourlyStockData[]  timeSeriesDaily;
+
+
+
+    public StockDetail[] getMetaData()
+    {
+        return metaData;
+    }
+
+    public HourlyStockData[] getTimeSeriesDaily()
+    {
+        return timeSeriesDaily;
+    }
 }
