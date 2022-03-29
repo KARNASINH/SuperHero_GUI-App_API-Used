5f13d5603f201813e69ffb5ef3b8d797c34cf0c7
KARNASINH <karnasinh281@gmail.com>
2022-03-28 22:29:07 -0400

    Created an StockData class.

---
+++ src/main/java/com/example/assignment2gc200478377/HourlyStockData.java
@@ -0,0 +1,33 @@
+package com.example.assignment2gc200478377;
+
+public class HourlyStockData
+{
+    private float open, high, low, close;
+
+    private int volume;
+
+    public float getOpen()
+    {
+        return open;
+    }
+
+    public float getHigh()
+    {
+        return high;
+    }
+
+    public float getLow()
+    {
+        return low;
+    }
+
+    public float getClose()
+    {
+        return close;
+    }
+
+    public int getVolume()
+    {
+        return volume;
+    }
+}
