833d6b02abfcc7a86f238db1691767ebcd3c7021
KARNASINH <karnasinh281@gmail.com>
2022-04-05 08:52:14 -0400

    JSON result Object's fields created with getter.

--- src/main/java/com/example/assignment2gc200478377/Result.java
+++ src/main/java/com/example/assignment2gc200478377/Result.java
@@ -2,4 +2,22 @@ package com.example.assignment2gc200478377;
 
 public class Result
 {
+    private String id;
+    private String name;
+    private Powerstats powerstats;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Powerstats getPowerstats()
+    {
+        return powerstats;
+    }
 }
