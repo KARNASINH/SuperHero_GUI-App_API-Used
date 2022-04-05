7b5e64d876c7f7090f7cc5a7fcf69c3a96c306ed
KARNASINH <karnasinh281@gmail.com>
2022-04-05 09:55:51 -0400

    Powerstats class finished.

--- src/main/java/com/example/assignment2gc200478377/Powerstats.java
+++ src/main/java/com/example/assignment2gc200478377/Powerstats.java
@@ -2,4 +2,34 @@ package com.example.assignment2gc200478377;
 
 public class Powerstats
 {
+    private String intelligence;
+    private String strength;
+    private String speed;
+    private String durability;
+    private String power;
+    private String combat;
+
+    public String getIntelligence() {
+        return intelligence;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public String getDurability() {
+        return durability;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public String getCombat() {
+        return combat;
+    }
 }
