4d22473147962fded62efd2a152261a21116326a
KARNASINH <karnasinh281@gmail.com>
2022-04-07 13:06:06 -0400

    Superhero search scenebuilder is created.

--- src/main/java/com/example/assignment2gc200478377/SearchSuperheroViewController.java
+++ src/main/java/com/example/assignment2gc200478377/SearchSuperheroViewController.java
@@ -1,16 +1,30 @@
 package com.example.assignment2gc200478377;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-public class SearchSuperheroViewController
-{
-    @FXML
-    private Label welcomeText;
+public class SearchSuperheroViewController {
 
     @FXML
-    protected void onHelloButtonClick()
-    {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button getDetailsButton;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private ListView<Result> listView;
+
+    @FXML
+    private Label notFoundLabel;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField searchTextField;
+
 }
