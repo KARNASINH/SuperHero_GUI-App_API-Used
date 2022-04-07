ca98d0a97b013a7f677ec207d5474827fbb398aa
KARNASINH <karnasinh281@gmail.com>
2022-04-07 08:58:19 -0400

    Updated logo and controller class name for the application.

--- src/main/java/com/example/assignment2gc200478377/Main.java
+++ src/main/java/com/example/assignment2gc200478377/Main.java
@@ -3,31 +3,34 @@ package com.example.assignment2gc200478377;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main
-    //extends Application
+public class Main extends Application
 {
-/*
+
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hourly_stock_data.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-superhero-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Search your Superhero");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:src/main/resources/com/example/assignment2gc200478377/favicon.png"));
         stage.show();
     }
-*/
+
     public static void main(String[] args)
     {
-        //launch();
-
+        launch();
+/*
         System.out.println();
 
         ApiResponse api = APIUtility.getSearchFromAPI("man");
         System.out.println(api);
+
+ */
     }
 }
--- src/main/java/com/example/assignment2gc200478377/SearchSuperheroViewController.java
+++ src/main/java/com/example/assignment2gc200478377/SearchSuperheroViewController.java
@@ -3,7 +3,7 @@ package com.example.assignment2gc200478377;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class StockSearchViewController
+public class SearchSuperheroViewController
 {
     @FXML
     private Label welcomeText;
