795ac03bcbf1f84e7aeb49402c3e0b74473301a1
KARNASINH <karnasinh281@gmail.com>
2022-04-07 16:31:43 -0400

    Prepared skeleton for SuperheroDetailsViewController class.

--- src/main/java/com/example/assignment2gc200478377/SuperheroDetailsViewController.java
+++ src/main/java/com/example/assignment2gc200478377/SuperheroDetailsViewController.java
@@ -1,5 +1,57 @@
 package com.example.assignment2gc200478377;
 
-public class SuperheroDetailsViewController
-{
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class SuperheroDetailsViewController {
+
+    @FXML
+    private Label combatLabel;
+
+    @FXML
+    private Label durabilityLabel;
+
+    @FXML
+    private Label eyeColourLabel;
+
+    @FXML
+    private Label genderLabel;
+
+    @FXML
+    private Label hairColourLabel;
+
+    @FXML
+    private Label heightCmLabel;
+
+    @FXML
+    private Label heightInchLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label intelligenceLabel;
+
+    @FXML
+    private Label powerLabel;
+
+    @FXML
+    private Label raceLabel;
+
+    @FXML
+    private Label speedLabel;
+
+    @FXML
+    private Label strengthLabel;
+
+    @FXML
+    private Label superheroTitleLabel;
+
+    @FXML
+    private Label weightKgLabel;
+
+    @FXML
+    private Label weightLbLabel;
+
 }
