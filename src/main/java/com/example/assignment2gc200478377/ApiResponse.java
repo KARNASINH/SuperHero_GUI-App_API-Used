30799e66dec2dd87ba8dfb1cf52f5769b25c8b18
KARNASINH <karnasinh281@gmail.com>
2022-04-08 13:34:33 -0400

    Comments added to ApiResponse class.

--- src/main/java/com/example/assignment2gc200478377/ApiResponse.java
+++ src/main/java/com/example/assignment2gc200478377/ApiResponse.java
@@ -1,9 +1,15 @@
+//Imported different classes to use their functionality.
 package com.example.assignment2gc200478377;
 
+/**
+ * This Class received API response in JSON format and map its object with defined variable name.
+ */
 public class ApiResponse
 {
+    //Declared instance field to map JSON object.
     private Result[] results;
 
+    //Getter method to get the data.
     public Result[] getResults() {
         return results;
     }
