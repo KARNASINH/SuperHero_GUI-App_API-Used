eb51b714f75106ce974d012f481b5195f502d59d
KARNASINH <karnasinh281@gmail.com>
2022-04-08 14:30:10 -0400

    Comments added to SceneChanger class.

--- src/main/java/com/example/assignment2gc200478377/SceneChanger.java
+++ src/main/java/com/example/assignment2gc200478377/SceneChanger.java
@@ -1,3 +1,4 @@
+//This class is a part of this package
 package com.example.assignment2gc200478377;
 
 import javafx.event.ActionEvent;
@@ -10,27 +11,42 @@ import java.io.IOException;
 
 public class SceneChanger
 {
+    /**
+     * This is static method to change scene from Superhero search scene to Superhero detail scene.
+     * @param fxmlFile This argument pass fxml file name to the method.
+     * @param id This argument pass Superhero ID to the method and get data based on this ID.
+     */
     public static void changeSceneWithId(ActionEvent event, String fxmlFile, String id) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
         Scene scene = new Scene(fxmlLoader.load());
 
+        //Created a SuperheroDetailsViewController object.
         SuperheroDetailsViewController controller = fxmlLoader.getController();
+        //Calling static method and display data on the scene.
         controller.loadSuperheroInfo(id, event);
 
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(scene);
-        stage.setTitle("Character Id: " + id);
+        //Setting up title of the scene page.
+        stage.setTitle("Character details for Id: " + id);
+        //Publishing stage
         stage.show();
     }
 
+    /**
+     * This is a static method to change scene from Superhero Detail scene to Superhero search scene.
+     * @param fxmlFile This argument pass fxml file name to the method.
+     */
     public static void changeScene(ActionEvent event, String fxmlFile) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
         Scene scene = new Scene(fxmlLoader.load());
 
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(scene);
+        //Setting up title of the scene page.
         stage.setTitle("Search your Superhero");
+        //Publishing stage
         stage.show();
     }
 }
