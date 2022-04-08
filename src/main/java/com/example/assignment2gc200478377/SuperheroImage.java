ba562357049751a51ec334c695873960bc44cd28
KARNASINH <karnasinh281@gmail.com>
2022-04-08 14:15:30 -0400

    Comments added to SuperheroImage class.

--- src/main/java/com/example/assignment2gc200478377/SuperheroImage.java
+++ src/main/java/com/example/assignment2gc200478377/SuperheroImage.java
@@ -1,9 +1,15 @@
+//This class is a part of this package
 package com.example.assignment2gc200478377;
 
+/**
+ * This class store all appearance of superhero into respective fields and returns them when getter method is called.
+ */
 public class SuperheroImage
 {
+    //Declaring a field to store values in them.
     private String url;
 
+    //Getter method to access private field.
     public String getUrl()
     {
         return url;
