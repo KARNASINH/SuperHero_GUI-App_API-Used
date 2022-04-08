13d0d48a1312e45a2b2497221a38780cf09765f3
KARNASINH <karnasinh281@gmail.com>
2022-04-08 14:14:36 -0400

    Comments added to Result class.

--- src/main/java/com/example/assignment2gc200478377/Result.java
+++ src/main/java/com/example/assignment2gc200478377/Result.java
@@ -1,13 +1,19 @@
+//This class is a part of this package
 package com.example.assignment2gc200478377;
 
+/**
+ * This class store all topmost Result object JSON data into respective fields and returns them when getter method is called.
+ */
 public class Result
 {
+    //Declaring different field to store values in them.
     private String id;
     private String name;
     private Powerstats powerstats;
     private Appearance appearance;
     private SuperheroImage image;
 
+    //Getter method to access private field.
     public String getId()
     {
         return id;
@@ -33,6 +39,7 @@ public class Result
         return image;
     }
 
+    //This will return string of Superhero ID and name together.
     public String toString()
     {
         return String.format(id + "-" + name);
